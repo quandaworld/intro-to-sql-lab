dc85856a7da2c545a070505f17ff7f513d4340fe
quandaworld <nhiquannguyen@gmail.com>
2025-04-17 12:14:52 -0700

    Update clue 3 solution

--- clues.sql
+++ clues.sql
@@ -23,12 +23,10 @@ WHERE countries.name = 'Holy See (Vatican City State)' AND isofficial = 'T';
 SELECT countries.name
 FROM countries
 JOIN countrylanguages ON countries.code = countrylanguages.countrycode
-GROUP BY countries.name
-HAVING 
-    SUM(CASE WHEN countrylanguages.language = 'Italian' AND countrylanguages.isofficial = 'T' THEN 1 ELSE 0 END) = 1
-    AND COUNT(countrylanguages.language) = 1
+WHERE countrylanguages.language = 'Italian' 
+    AND countrylanguages.isofficial = 'T'
+    AND countrylanguages.percentage = 100.0
     AND countries.name != 'Holy See (Vatican City State)';
-
 
 -- Clue #4: We're booking the first flight out – maybe we've actually got a chance to catch her this time. There are only two cities she could be flying to in the country. One is named the same as the country – that would be too obvious. We're following our gut on this one; find out what other city in that country she might be flying to.
 
